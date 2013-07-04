bc481898b79bce84559d39ac1bb6c0e8713d6b73
Jurriën Dokter <jurrien@jpdokter.nl>
2013-07-04 16:39:24 +0200

    added pubsub

--- init.py
+++ init.py
@@ -7,11 +7,13 @@ from pprint             import pprint
 from twisted.internet   import reactor
 from twisted.python     import log
 from autobahn.websocket import WebSocketClientFactory, WebSocketClientProtocol, connectWS
+from pubsub             import pub
 
 class ScalableServerProtocol(WebSocketClientProtocol):
     _connected = False
     _user = 'Oby-chan'
     _history = Stack(True)
+    _use_events = False
 
     def parseMessage(self, message):
 
@@ -23,7 +25,6 @@ class ScalableServerProtocol(WebSocketClientProtocol):
 
         if( type(message) != dict):
             message = {
-                'user': _user,
                 'message': message
             }
 
@@ -31,14 +32,13 @@ class ScalableServerProtocol(WebSocketClientProtocol):
 
         return message
 
+
     def send(self, message):
         message = self.parseMessage(message)
 
-        if(_history.contains(message) == False):
-
-            text = json.dump(message)
-            self.sendMessage(text)
-            _history.add(message)
+        text = json.dump(message)
+        self.sendMessage(text)
+        self._history.add(message)
 
     def dump(self, message):
         pprint(message)
@@ -51,10 +51,24 @@ class ScalableServerProtocol(WebSocketClientProtocol):
     def onMessage(self, msg, binary):
         parsedMessage = self.parseMessage(msg)
 
-        if(_history.contains(parsedMessage) == False):
-            self.dump(parsedMessage)
-            _history.add(parsedMessage)
+        if(self._history.contains(parsedMessage) == False):
+            self._history.add(parsedMessage)
 
+            if("event" in parsedMessage.keys() and self._use_events == True):
+                self.trigger("evt." + parsedMessage['event'], parsedMessage)
+            else:
+                self.trigger("message", parsedMessage)
+
+    # enddef
+
+    def trigger(self, event, data):
+
+        # data = dict
+        # event = string
+        
+        pub.sendMessage(event, msg=data)
+
+# ----------------------------------------------------------------------------
 
 if __name__ == '__main__':
 
@@ -62,6 +76,15 @@ if __name__ == '__main__':
     port = 8800
 
     address = 'ws://' + host + ':' + str(port)
+
+    def pdump(msg):
+        if("event" in msg.keys()):
+            print "Event: %s", msg['event']
+        
+        print msg['user'], ': ', msg['message']
+
+    pub.subscribe(pdump, 'message')
+
 
     factory = WebSocketClientFactory(address, debug = True, debugCodePaths = True)
     factory.protocol = ScalableServerProtocol
