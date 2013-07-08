fb9d41189aeea270ee110717e1936f98b057f1a0
bloodywing <bloodywing@neocomy.net>
2013-07-08 15:38:52 +0200

    added a basic setup.py moved file to new location
    added a basic setup.py, needs to be completed

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="ss-client",
+      version="9999",
+      description="Dummy",
+      author="jpdokter",
+      author_email="",
+      url="https://github.com/WickedSik/python-ss-client",
+      packages=["ss-client"],
+      license = "",
+      install_requires=["twisted", "autobahn", "pubsub"])
